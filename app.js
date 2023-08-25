3a3295b00ceba4756eca8a0172ed126d4bd76446
matthew-2000 <matteo.ercolino01@gmail.com>
2023-08-25 11:31:27 +0200

    Added insertFilm page

--- app.js
+++ app.js
@@ -16,16 +16,33 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/insertFilm', (req, res) => {
+    res.sendFile(__dirname + '/public/insertFilm.html');
+});
+
 app.get('/items', async (req, res) => {
     const items = await db.getItems();
     res.json(items);
   });
 
-  app.post('/addItem', async (req, res) => {
-    const newItem = { name: req.body.name };
-    await db.addItem(newItem);
+app.post('/addItem', async (req, res) => {
+    const newMovie = {
+        name: req.body.name,
+        info: req.body.info,
+        genres: req.body.genres,
+        directors: req.body.directors,
+        authors: req.body.authors,
+        release_date: req.body.release_date,
+        runtime: req.body.runtime,
+        production_company: req.body.production_company,
+        rotten_rating: req.body.rotten_rating,
+        netflix_rating: req.body.netflix_rating,
+        imdb_rating: req.body.imdb_rating,
+    };
+
+    await db.addItem(newMovie);
     res.redirect('/');
-  });
+});
   
   app.post('/removeItem', async (req, res) => {
     const itemId = require('mongodb').ObjectID(req.body.id);
