d3ab998a1b81cb464d83820f1c64e9d6fea5fd8e
ssilvestri15 <s.silvestri15@studenti.unisa.it>
2023-08-31 12:13:20 +0200

    Added filter in backend

--- app.js
+++ app.js
@@ -23,8 +23,30 @@ app.get('/updateFilm', (req, res) => {
 });
 
 app.get('/items', async (req, res) => {
-    const items = await db.getItems();
-    res.json(items);
+    try {
+        const genres = req.body.genres;
+        const release_date = req.body.release_date;
+        const directors = req.body.directors;
+        const type = req.body.type;
+        const platform = req.body.platform;
+        const rating_platform = req.body.rating_platform;
+        const rating_platform_value = req.body.rating_platform_value;
+        const filters = {
+            genres: genres === undefined ? "" : genres,
+            release_date:  release_date === undefined ? "" : release_date,
+            directors:  directors === undefined ? "" : directors,
+            type:  type === undefined ? "" : type,
+            platform:  platform === undefined ? "" : platform,
+            rating_platform:  rating_platform === undefined ? "" : rating_platform,
+            rating_platform_value:  rating_platform_value === undefined ? "" : rating_platform_value
+        };
+        console.log(filters)
+        const items = await db.getItems(filters);
+        res.json(items);
+    } catch (error) {
+        console.error('Errore durante il recupero dei film:', error);
+        res.status(500).send('Si è verificato un errore durante il recupero dei film.');
+    }
 });
 
 app.post('/addItem', async (req, res) => {
@@ -87,9 +109,19 @@ app.post('/updateItem', async (req, res) => {
 
 
 app.post('/removeItem', async (req, res) => {
-    const itemId = require('mongodb').ObjectID(req.body.id);
-    await db.removeItem(itemId);
-    res.redirect('/');
+    const itemId = req.body.id
+    try {
+        const result = await db.removeItem(itemId);
+
+        if (result.deletedCount === 1) {
+            console.log('Cancellazione avvenuta con successo.');
+            res.redirect('/');
+        } else {
+            console.log('Nessun item corrispondente trovato per la cancellazione.');
+        }
+    } catch (error) {
+        console.error('Si è verificato un errore durante la cancellazione:', error);
+    }
 });
 
 // Connect to MongoDB
--- db/mongodb.js
+++ db/mongodb.js
@@ -12,8 +12,57 @@ async function connect() {
   db = client.db(dbName);
 }
 
-function getItems() {
-  return db.collection('movies').find().toArray();
+function getItems(filters) {
+
+  console.log(filters);
+
+  if (!filters) {
+    return db.collection('movies').find().toArray();
+  }
+
+  if (filters.type === 'movie') {
+
+    const filterQuery = {};
+
+    if (filters.genres) {
+      const genresRegex = new RegExp(`.*${filters.genres}.*`, 'i');
+      filterQuery.genres = filters.genresRegex;
+    }
+
+    if (filters.release_date) {
+      filterQuery.release_date = { $regex: `^${filters.release_date}-` };
+    }
+
+    if (filters.directors) {
+      const directorsRegex = new RegExp(`.*${filters.directors}.*`, 'i');
+      filterQuery.directors = { $regex: directorsRegex};
+    }
+
+    return db.collection('movies').find(filterQuery).toArray();
+
+  } else {
+
+    const filterQuery = {};
+
+    if (filters.release_date) {
+      filterQuery.release_date = filters.release_date;
+    }
+
+    if (filters.platform) {
+      //check if is an array
+      filterQuery.platforms = { $in: [filters.platform] };
+    }
+
+    if (filters.rating_platform && filters.rating_platform_value) {
+      const platform = filters.rating_platform == 'rotten_tomatoes' ? 'rotten_rating' : 'imdb_rating';
+      const value_to_search = filters.rating_platform == 'rotten_tomatoes' ? `${filters.rating_platform_value}/100` : `${filters.rating_platform_value}/10`;
+      filterQuery[`ratings.${platform}`] = value_to_search;
+    }
+
+    console.log(filterQuery);
+    return db.collection('tv_shows').find(filterQuery).toArray();
+  }
+
 }
 
 function addItem(item) {
